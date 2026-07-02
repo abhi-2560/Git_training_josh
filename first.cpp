8f5401f11f56ae38f168f87e069ef604df7620f0
abhi-2560 <shukla.abhijeet544@gmail.com>
2026-07-02 13:20:40 +0530

    feat:created linkedList

--- first.cpp
+++ first.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
 using namespace std;
 
-struct Node {
+struct Node
+{
     int data;
-    Node* next;
+    Node *next;
 };
 
-int main(){
-    cout<<"hello"<<endl;
+int main()
+{
+    // Create linked list: 1 -> 2 -> 3 -> 4
+    Node *head = new Node{1, nullptr};
+    head->next = new Node{2, nullptr};
+    head->next->next = new Node{3, nullptr};
+    head->next->next->next = new Node{4, nullptr};
 }
